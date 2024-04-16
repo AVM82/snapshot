ff67dc7f432999109c490b65c041bbd9705bfc80
OlenaMovchan <elenawju@gmail.com>
2024-04-16 15:13:36 +0300

    added an empty Spring Boot project

---
+++ snapshot-spring-boot/src/main/java/com/project/snapshotspringboot/SnapshotSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project.snapshotspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnapshotSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnapshotSpringBootApplication.class, args);
+    }
+
+}
---
+++ snapshot-spring-boot/src/test/java/com/project/snapshotspringboot/SnapshotSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.snapshotspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnapshotSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
