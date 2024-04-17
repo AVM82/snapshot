c4ba3977ea4589ee7de419a96641c91538dfcf72
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-04-17 14:18:42 +0300

    .

--- snapshot-react/src/App.tsx
+++ snapshot-react/src/App.tsx
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 import './App.scss';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import reactLogo from './assets/react.svg';
+import snapshotApi from './api/request';
 
 export default function App(): JSX.Element {
   const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const sayHello = async () => {
+      const response = await snapshotApi.get('http://localhost:5173/users/hello');
+      console.log(response);
+    }
+
+    sayHello();
+  })
 
   return (
     <>
