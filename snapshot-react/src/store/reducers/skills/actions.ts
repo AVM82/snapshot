bd2b9f1cd73c98d57434c3cbcbeb096b319b3394
Max_Shulga <shulgamackim@gmail.com>
2024-04-25 16:33:34 +0300

    refactoring

--- snapshot-react/src/store/reducers/skills/actions.ts
+++ snapshot-react/src/store/reducers/skills/actions.ts
@@ -4,11 +4,8 @@ import snapshotApi from '../../../api/request';
 import { ISkills } from '../../../models/profile/ISkills';
 import ActionType from './common';
 
-const getAllSkills = createAsyncThunk(
-  ActionType.GET_ALL_SKILLS,
-  async (): Promise<ISkills[]> => snapshotApi.get('http://localhost:8080/skills/role/0'));
 const getRoleSkills = createAsyncThunk(
   ActionType.GET_ROLE_SKILLS,
   async (id:string):Promise<ISkills[]> => snapshotApi.get(`http://localhost:8080/skills/role/${id}`),
 );
-export { getAllSkills, getRoleSkills };
+export default getRoleSkills;
