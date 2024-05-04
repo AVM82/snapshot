704b592f4cc3ad0819ffab7669dc61d0483e3fe7
Max_Shulga <shulgamackim@gmail.com>
2024-05-04 12:51:04 +0300

    fix getUser action in interviewSlice

--- snapshot-react/src/pages/Profile/components/InterviewActionPanel/InterviewActionPanel.tsx
+++ snapshot-react/src/pages/Profile/components/InterviewActionPanel/InterviewActionPanel.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useAppDispatch } from '../../../../hooks/redux';
+import { resetInterviewState } from '../../../../store/reducers/interwiew/interviewSlice';
 import styles from '../../MyInterviews.module.scss';
 import InterviewScheduler from '../InterviewScheduler/InterviewScheduler';
 
@@ -8,15 +10,18 @@ function InterviewActionPanel():React.JSX.Element {
   const [showNewInterview, setShowNewInterview] = useState(false);
   const [showInterviewScheduler, setShowInterviewScheduler] = useState(false);
   const handleNewInterviewClick = ():void => setShowNewInterview(true);
-
+  const dispatch = useAppDispatch();
   const handleSchedulerClose = ():void => {
     setShowInterviewScheduler(false);
     setShowNewInterview(false);
   };
+  const handleLinkToInterview = ():void => {
+    dispatch(resetInterviewState());
+  };
 
   const renderNewInterviewBlock = ():React.JSX.Element => (
     <div className={styles.NewInterviewBlock}>
-      <button type="button">
+      <button type="button" onClick={handleLinkToInterview}>
         <Link to="/interview" className={styles.Link}>
           Розпочати
         </Link>
