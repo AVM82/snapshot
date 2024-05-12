68afb5effd65bc19a6ac7bf2d38cac5ac5f326b6
OlenaMovchan <elenawju@gmail.com>
2024-05-12 19:30:01 +0300

    add interview reminders via interviewer and searcher email

--- snapshot-spring-boot/src/main/java/com/project/snapshotspringboot/repository/InterviewRepository.java
+++ snapshot-spring-boot/src/main/java/com/project/snapshotspringboot/repository/InterviewRepository.java
@@ -1,6 +1,7 @@
 package com.project.snapshotspringboot.repository;
 
 import com.project.snapshotspringboot.entity.InterviewEntity;
+import com.project.snapshotspringboot.enumeration.InterviewStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +12,6 @@ public interface InterviewRepository extends JpaRepository<InterviewEntity, Long
     List<InterviewEntity> findBySearcherId(Long id);
 
     List<InterviewEntity> findBySearcherIdAndEndDateTimeBetween(Long searcherId, LocalDateTime from, LocalDateTime to);
+
+    List<InterviewEntity> findByStatusAndPlannedDateTimeBetween(InterviewStatus interviewStatus, LocalDateTime tomorrow, LocalDateTime endOfTomorrow);
 }
