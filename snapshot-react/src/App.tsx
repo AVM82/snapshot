c66ae09a9932b55bdc5a31025a25d3128ab247a6
Max_Shulga <shulgamackim@gmail.com>
2024-05-17 23:16:16 +0300

    snap-36(brought the registration page to design)

--- snapshot-react/src/App.tsx
+++ snapshot-react/src/App.tsx
@@ -1,12 +1,15 @@
 import './App.scss';
 
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import AuthPage from './pages/Auth/AuthPage';
+import ForgotPassword from './pages/Auth/ForgotPassword';
 import OAuth2RedirectHandler from './pages/Auth/OAuth2RedirectHandler';
 import SignInPage from './pages/Auth/SignInPage';
 import SignUpPage from './pages/Auth/SignUpPage';
+import SuccessfulSignUp from './pages/Auth/SuccessfulSignUp';
 import CandidateSearch from './pages/CandidateSearch/CandidateSearch';
 import Home from './pages/Home/Home';
 import InterviewPage from './pages/Interview/InterviewPage';
@@ -16,27 +19,43 @@ import Profile from './pages/Profile/Profile';
 import UserRoles from './pages/Profile/UserRoles';
 import ProtectedRoute from './utils/ProtectedRoute';
 
-function App(): JSX.Element {
+function App(): React.JSX.Element {
   return (
     <Routes>
+      <Route
+        path="sign-in"
+        element={(
+          <AuthPage>
+            <SignInPage />
+          </AuthPage>
+        )}
+      />
+      <Route
+        path="forgot-password"
+        element={(
+          <AuthPage>
+            <ForgotPassword />
+          </AuthPage>
+        )}
+      />
+      <Route
+        path="sign-up"
+        element={(
+          <AuthPage>
+            <SignUpPage />
+          </AuthPage>
+        )}
+      />
+      <Route
+        path="thank-you"
+        element={(
+          <AuthPage>
+            <SuccessfulSignUp />
+          </AuthPage>
+        )}
+      />
       <Route path="/" element={<Layout />}>
         <Route element={<Home />} index />
-        <Route
-          path="sign-in"
-          element={(
-            <AuthPage>
-              <SignInPage />
-            </AuthPage>
-          )}
-        />
-        <Route
-          path="sign-up"
-          element={(
-            <AuthPage>
-              <SignUpPage />
-            </AuthPage>
-          )}
-        />
         <Route path="oauth2/redirect" element={<OAuth2RedirectHandler />} />
         <Route path="candidate-search" element={<CandidateSearch />} />
         <Route element={<ProtectedRoute />}>
