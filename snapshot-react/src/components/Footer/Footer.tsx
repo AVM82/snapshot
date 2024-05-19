9a83db764be801c2f450db0438848f67e8dcd7b4
OlenaMovchan <elenawju@gmail.com>
2024-05-20 00:24:42 +0300

    test

--- snapshot-react/src/components/Footer/Footer.tsx
+++ snapshot-react/src/components/Footer/Footer.tsx
@@ -1,12 +1,26 @@
+import styles from './Footer.module.scss';
+import iconIn from '../../assets/icon-in.svg';
+import iconF from '../../assets/icon-f.svg';
+import iconTv from '../../assets/icon-tv.svg';
+import classNames from 'classnames';
 
-function Footer(): JSX.Element {
+function Footer(): React.JSX.Element {
   return (
-    <footer style={{
-      height: '150px', fontSize: '32px', textAlign: 'center', backgroundColor: 'aqua',
-    }}
-    >
-      Footer
-    </footer>
+    <footer className={styles.footer}>
+      <div className={styles.footer_linksContainer}>
+        <div className={classNames(styles.footer_link, styles.footer_link_hr)}>HR</div>
+        <div className={styles.footer_link}>Співбесіда</div>
+        <div className={styles.footer_link}>Довідник скілів</div>
+      </div>
+      <div className={styles.footer_socialMediaLinks}>
+        <img src={iconF} alt="Facebook icon" />
+        <img src={iconTv} alt="Twitter icon" />
+        <img src={iconIn} alt="LinkedIn icon" />
+      </div>
+      <div className={styles.footer_copyright}>
+        SNAPSHOT IT © 2024. All rights reserved.
+      </div>
+    </footer >
   );
 }
 
---
+++ snapshot-react/src/components/Header/HeaderSignIn.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styles from './Header.module.scss';
+
+import { useAppSelector } from '../../hooks/redux';
+import useForce from '../../utils/useForce';
+import classNames from 'classnames';
+
+function HeaderSignIn(): React.JSX.Element {
+  const user = useAppSelector((state) => state.user.userData);
+  const location = useLocation();
+  useForce(location);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header_top}>
+        <div className={styles.header_top_location}>Україна</div>
+        <div className={styles.header_top_buttonBar}>
+          <Link to="sign-in" className={classNames(styles.header_top_button_signin, styles.header_link)}>УВІЙТИ</Link>
+          <div className={styles.header_top_buttonBar_line}></div>
+          <Link to="sign-up" className={classNames(styles.header_top_button_signup, styles.header_link)}>ЗАРЕЄСТРУВАТИСЬ</Link>
+        </div>
+      </div>
+      <div className={styles.header_title}>SNAPSHOT IT</div>
+    </header>
+  );
+}
+
+export default HeaderSignIn;
