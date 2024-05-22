f3a012bfd150ca6088ed80a1a18422c0e466aa58
Danylo Shlapak <geografy2314@gmail.com>
2024-05-22 23:25:23 +0300

    apply stylelint

--- snapshot-react/src/pages/Profile/components/Interviews/components/InterviewScheduler/InterviewScheduler.tsx
+++ snapshot-react/src/pages/Profile/components/Interviews/components/InterviewScheduler/InterviewScheduler.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
-import {useAppDispatch, useAppSelector} from '../../../../../../hooks/redux';
-import {INewInterview} from '../../../../../../models/profile/INewInterview';
-import {addInterview, getUserByEmail} from '../../../../../../store/reducers/interwiew/actions';
+import { useAppDispatch, useAppSelector } from '../../../../../../hooks/redux';
+import { INewInterview } from '../../../../../../models/profile/INewInterview';
+import { addInterview, getUserByEmail } from '../../../../../../store/reducers/interwiew/actions';
 import styles from './InterviewScheduler.module.scss';
 
 interface CustomProps {
