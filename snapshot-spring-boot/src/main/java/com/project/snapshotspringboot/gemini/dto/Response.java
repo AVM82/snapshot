1cc63945e3a7c9026919d93899497ca636387e06
pavel.y.bondarenko <pavel.y.bondarenko@gmail.com>
2024-05-22 20:31:39 +0300

    change gemini ai logic. add cross service in US.

--- snapshot-spring-boot/src/main/java/com/project/snapshotspringboot/gemini/dto/Response.java
+++ snapshot-spring-boot/src/main/java/com/project/snapshotspringboot/gemini/dto/Response.java
@@ -3,11 +3,8 @@ package com.project.snapshotspringboot.gemini.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class Response {
-    private List<Candidate> candidates;
-    private UsageMetadata usageMetadata;
+    private String answer;
 }
