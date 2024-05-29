1283daef3ab5f690917099b797235b9d913d9259
pavel.y.bondarenko <pavel.y.bondarenko@gmail.com>
2024-05-29 17:33:02 +0300

    edit timer

--- snapshot-react/src/pages/Interview/components/Timer/Timer.tsx
+++ snapshot-react/src/pages/Interview/components/Timer/Timer.tsx
@@ -7,19 +7,36 @@ type CombinedProps = React.HTMLProps<HTMLDivElement>;
 
 function Timer(rest: CombinedProps): React.JSX.Element {
   const [time, setTime] = useState(0);
-  const { status: interviewStatus } = useAppSelector((state) => state.interview);
+  const { status: interviewStatus, startDateTime, endDateTime } = useAppSelector((state) => state.interview);
+
   useEffect(() => {
-    const intervalId = setInterval(() => setTime(time + 1), 10);
+    const changeTime = (): void => {
 
-    return (): void => clearInterval(intervalId);
-  }, [time]);
+      if (startDateTime) {
+        const endMillis = endDateTime
+          ? new Date(endDateTime).getTime()
+          : new Date().getTime() + new Date().getTimezoneOffset() * 60000;
+        const timeMillis = endMillis - new Date(startDateTime).getTime();
 
-  const hours = Math.floor(time / 360000);
-  const minutes = Math.floor((time % 360000) / 6000);
-  const seconds = Math.floor((time % 6000) / 100);
-  const clock = interviewStatus === 'ACTIVE'
-    ? `${hours ? `${hours}:` : ''} ${minutes >= 10 ? minutes : `0${minutes}`}:${seconds >= 10 ? seconds : `0${seconds}`}`
-    : '00:00';
+        setTime(timeMillis);
+      }
+    };
+
+    changeTime();
+
+    if (interviewStatus === 'ACTIVE') {
+      const intervalId = setInterval(changeTime, 200);
+
+      return (): void => clearInterval(intervalId);
+    }
+
+    return (): void => { };
+  }, [interviewStatus, startDateTime, endDateTime]);
+
+  const hours = Math.floor(time / 3600000);
+  const minutes = Math.floor((time % 3600000) / 60000);
+  const seconds = Math.floor((time % 60000) / 1000);
+  const clock = `${hours ? `${hours}:` : ''} ${minutes >= 10 ? minutes : `0${minutes}`}:${seconds >= 10 ? seconds : `0${seconds}`}`;
 
   return (
     <div {...rest}>
